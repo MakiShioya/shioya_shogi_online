4e23073b2870e271ccaf85bc9680108d95fd0ad6
MakiShioya <makimakisioya@gmail.com>
2026-02-06 18:23:11 +0900

    Update items.js

--- public/script/items.js
+++ public/script/items.js
@@ -47,6 +47,35 @@ const GAME_ITEMS = [
     placeholderText: "黒木",
     desc: "重厚感のある暗い色の木製盤です。"
   }
+
+  {
+    id: "bgm_default",
+    name: "いつもの曲",
+    price: 0,
+    type: "bgm",
+    src: "script/audio/natsu2.mp3", // デフォルトの曲パス
+    desc: "標準のBGMです。"
+  },
+  {
+    id: "bgm_rock",
+    name: "激闘ロック",
+    price: 1500,
+    type: "bgm",
+    src: "script/audio/rock.mp3", // ★新しく用意したmp3ファイルのパス
+    desc: "激しい戦いを予感させるロックな曲です。",
+    // ショップで画像がないと寂しいので、音符マークなどの画像を指定すると良いです
+    image: "script/image/music_icon.png" 
+  },
+  {
+    id: "bgm_piano",
+    name: "静かなピアノ",
+    price: 2000,
+    type: "bgm",
+    src: "script/audio/piano.mp3",
+    desc: "集中力を高める静かなピアノ曲です。",
+    image: "script/image/music_icon.png"
+  }
+
 ];
 
 // アイテムIDからデータを探すヘルパー関数
@@ -54,3 +83,4 @@ function getItemById(id) {
   if (typeof GAME_ITEMS === 'undefined') return null;
   return GAME_ITEMS.find(item => item.id === id);
 }
+
