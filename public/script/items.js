72eb821a08aeb6e02bff8f022e83e17e603b3239
MakiShioya <makimakisioya@gmail.com>
2026-02-06 18:37:50 +0900

    Update items.js

--- public/script/items.js
+++ public/script/items.js
@@ -15,16 +15,16 @@ const GAME_ITEMS = [
   },
   {
     id: "piece_dot",
-    name: "ドット絵の駒",
-    price: 1000,
+    name: "駒２",
+    price: 99999,
     type: "piece",
     image: "script/image/dot_preview.png",
     desc: "レトロゲーム風のドット絵駒です。"
   },
   {
     id: "piece_gold_style",
-    name: "黄金の駒",
-    price: 5000,
+    name: "駒３",
+    price: 99999,
     type: "piece",
     placeholderText: "金",
     desc: "成金趣味全開の黄金に輝く駒です。"
@@ -43,15 +43,15 @@ const GAME_ITEMS = [
   {
     id: "board_default",
     name: "ヨーロッパの標識",
-    price: 0,
+    price: 1000,
     type: "board",
     image: "script/image/hyoshiki.PNG",
     desc: "おなじみの標識"
   },
   {
     id: "board_wood_dark",
-    name: "黒檀の将棋盤",
-    price: 2500,
+    name: "将棋盤３",
+    price: 99999,
     type: "board",
     placeholderText: "黒木",
     desc: "重厚感のある暗い色の木製盤です。"
@@ -67,8 +67,8 @@ const GAME_ITEMS = [
   },
   {
     id: "bgm_rock",
-    name: "激闘ロック",
-    price: 1500,
+    name: "曲２",
+    price: 99999,
     type: "bgm",
     src: "script/audio/rock.mp3", // ★新しく用意したmp3ファイルのパス
     desc: "激しい戦いを予感させるロックな曲です。",
@@ -77,8 +77,8 @@ const GAME_ITEMS = [
   },
   {
     id: "bgm_piano",
-    name: "静かなピアノ",
-    price: 2000,
+    name: "曲３",
+    price: 99999,
     type: "bgm",
     src: "script/audio/piano.mp3",
     desc: "集中力を高める静かなピアノ曲です。",
@@ -92,6 +92,7 @@ function getItemById(id) {
   if (typeof GAME_ITEMS === 'undefined') return null;
   return GAME_ITEMS.find(item => item.id === id);
 }
+
 
 
 
